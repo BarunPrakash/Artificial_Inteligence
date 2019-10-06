e55d9632231f7083218833abcbf424892137b6db
BarunPrakash <brnprksh@gmail.com>
2019-10-05 22:31:03 -0700

    main() func

---
+++ Machine_Learning_Algo_Math/LinearReg.py
@@ -0,0 +1,26 @@
+"""
+Liner regressoin designed by barun
+Date : 10 4/2019
+
+"""
+#!/usr/bin/python
+from lrAlgo import *
+
+
+
+def  main():
+    #if (0):  # debug
+     #   forwardVal(1)
+     # randomLoss(2,4)
+        
+    computelossForDiffrentWeight()
+    print("Main")
+
+if __name__ == "__main__":
+    main()
+    
+
+
+ 
+    
+    
