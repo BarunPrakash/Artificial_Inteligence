3630ae1956a6b830a1a849dfc3b1ef747d3ceeaa
BarunPrakash <brnprksh@gmail.com>
2019-10-06 09:56:17 -0700

    EX_2_grad

---
+++ Machine_Learning_Algo_Math/mainText.py
@@ -0,0 +1,27 @@
+"""
+Liner regressoin designed by barun
+Date : 10 4/2019
+
+"""
+#!/usr/bin/python
+from lrAlgo import *
+
+
+
+def  main():
+    #if (0):  # debug
+     #   forwardVal(1)
+     # randomLoss(2,4)
+       # waiit!!
+    #computelossForDiffrentWeight()
+    findGradient()
+    print("Main")
+
+if __name__ == "__main__":
+    main()
+    
+
+
+ 
+    
+    
