8cafc928408769cc138404b9d23d54d1c3381aaa
BarunPrakash <brnprksh@gmail.com>
2019-10-12 21:01:02 -0700

    numpy test

---
+++ Machine_Learning/LibraryUtility/numpy/tensor_Matrix.py
@@ -0,0 +1,22 @@
+"""
+
+Cross entropy 
+Designed by Barun.
+Exploring Deep Learnning
+Date: 12 oct  2019
+
+""" 
+
+import torch
+import numpy as np
+
+
+
+l1 =[1,2,3,5]
+l2 =[1,2,3,5]
+x = np.array([[1,2],[4,4]])
+y  = torch.from_numpy(x)
+
+
+#t1 =torch.Tensor([l1,l2])
+print(x)
