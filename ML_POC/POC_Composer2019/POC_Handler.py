d6aaf3758323c6adb4ffe46b579eadd5ed73ce65
BarunPrakash <brnprksh@gmail.com>
2019-11-03 01:48:09 -0700

    Abstraction Layer

---
+++ ML_POC/POC_Composer2019/POC_Handler.py
@@ -0,0 +1,22 @@
+"""
+ POC Composer!!
+Date : 10/ 29/2019
+
+"""
+#!/usr/bin/python
+from ML_package_Accessor import POC_Accessor
+
+
+
+def  main():
+    POC_Accessor.read_DATA_FromXLSX()
+    print("Main")
+
+if __name__ == "__main__":
+    main()
+    
+
+
+ 
+    
+    
